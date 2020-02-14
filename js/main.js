486b52f4b5e8e395fa800113f5cfa754132dab1d
aliyevm111 <aliyevm111@gmail.com>
2020-02-14 08:05:04 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	// $('.iconsol').css('height',$('.slayd').css('height'))
+	$('.slaydlar').hover(function () {
+		$('.iconsol').css('height',$('.slayd').css('height'))
+		$('.iconsag').css('height',$('.slayd').css('height'))
+
+	})
+	// $('.iconsol').click(function () {
+	// 	$('.slayd').fadeTo(1000,0)
+
+	// })
+	var slideIndex = 0;
+	// $('.slaydlar .slayd').eq(0).fadeTo(1000,1)
+	// $('.slaydlar .slayd').eq(0).css('display','none')
+	var timeout = setTimeout(name1,7000)
+	function name1() {
+		if (slideIndex>=$('.slaydlar .slayd').length-1) {
+			slideIndex = 0;
+		}else{
+			slideIndex++;
+		}
+		name2();
+	}
+	function name2(){
+		$('.slaydlar .slayd').fadeTo(400,0)
+		$('.slaydlar .slayd').css('display','none')
+		$('.slaydlar .slayd').eq(slideIndex).fadeTo(400,1);
+		timeout = setTimeout(name1,7000);
+	}
+	$('.iconsol').click(function(x) {
+		clearTimeout(timeout);
+		if (slideIndex==0) {
+			slideIndex = $('.slaydlar .slayd').length-1;
+		}
+		else{
+			slideIndex--;
+		}
+		name2();
+	})
+	$('.iconsag').click(function(x) {
+		clearTimeout(timeout);
+		if (slideIndex==$('.slaydlar .slayd').length-1) {
+			slideIndex = 0;
+		}
+		else{
+			slideIndex++;
+		}
+		name2();	
+	})
+})

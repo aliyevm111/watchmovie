025e4c79125ab59c66e4d498806eab73d246a5fa
aliyevm111 <59919334+aliyevm111@users.noreply.github.com>
2020-02-19 23:50:38 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
+	var h=0
 	$('.iconsol').css('height',$('.slayd').css('height'))
 	$('.iconsag').css('height',$('.slayd').css('height'))
-	$('.slaydlar').hover(function () {
+	$('.op').css('width',$('.yuxari').css('width').slice(0,-2)-10+'px')
+	$('.op').css('margin-left','5px')
+	$('.op1').css('margin-top','10px')
+	h=$('.op1').css('height').slice(0,-2)
+	$('.op2').css('margin-top',h-(-20)+'px')
+	$('.op3').css('margin-top',$('.yuxari').css('height').slice(0,-2)-$('.op3').css('height').slice(0,-2)-10+'px')
+	$(window).resize(function () {
+		$('.op').css('width',$('.yuxari').css('width').slice(0,-2)-10+'px')
+		$('.op').css('margin-left','5px')
+		$('.op1').css('margin-top','10px')
+		h=$('.op1').css('height').slice(0,-2)
+		$('.op2').css('margin-top',h-(-20)+'px')
+		$('.op3').css('margin-top',$('.yuxari').css('height').slice(0,-2)-$('.op3').css('height').slice(0,-2)-10+'px')
 		$('.iconsol').css('height',$('.slayd').css('height'))
 		$('.iconsag').css('height',$('.slayd').css('height'))
-
 	})
+	// $('.asagi').hover()s
+
+
 	// $('.iconsol').click(function () {
 	// 	$('.slayd').fadeTo(1000,0)
 
 	// })
 	var slideIndex = 0;
+	var icaze = 1;
 	// $('.slaydlar .slayd').eq(0).fadeTo(1000,1)
 	// $('.slaydlar .slayd').eq(0).css('display','none')
 	var timeout = setTimeout(name1,7000)
@@ -25,27 +41,35 @@ $(document).ready(function() {
 	function name2(){
 		$('.slaydlar .slayd').fadeTo(400,0)
 		$('.slaydlar .slayd').css('display','none')
-		$('.slaydlar .slayd').eq(slideIndex).fadeTo(400,1);
+		$('.slaydlar .slayd').eq(slideIndex).fadeTo(400,1,function(){
+			icaze=1;
+		});
 		timeout = setTimeout(name1,7000);
 	}
 	$('.iconsol i').click(function(x) {
-		clearTimeout(timeout);
-		if (slideIndex==0) {
-			slideIndex = $('.slaydlar .slayd').length-1;
+		if (icaze==1) {
+			clearTimeout(timeout);
+			if (slideIndex==0) {
+				slideIndex = $('.slaydlar .slayd').length-1;
+			}
+			else{
+				slideIndex--;
+			}
+			icaze=0;
+			name2();
 		}
-		else{
-			slideIndex--;
-		}
-		name2();
 	})
 	$('.iconsag i').click(function(x) {
-		clearTimeout(timeout);
-		if (slideIndex==$('.slaydlar .slayd').length-1) {
-			slideIndex = 0;
+		if (icaze==1) {
+			clearTimeout(timeout);
+			if (slideIndex==$('.slaydlar .slayd').length-1) {
+				slideIndex = 0;
+			}
+			else{
+				slideIndex++;
+			}
+			icaze=0;
+			name2();	
 		}
-		else{
-			slideIndex++;
-		}
-		name2();	
 	})
 })
